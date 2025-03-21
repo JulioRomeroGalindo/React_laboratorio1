d81cb12109bf075108d10c51b1756f13a29f8cc8
JulioRomeroGalindo <juliocesarromerogalindo@gmail.com>
2025-03-21 00:07:42 -0400

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "/React_laboratorio1",
+    plugins: [react()],
 })
